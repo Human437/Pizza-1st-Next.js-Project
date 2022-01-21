8335784902ba723504f3e90e526f8af5f2f1a8d8
Ryan <leeyuenhigh@gmail.com>
2022-01-21 15:47:39 -0500

    Added a list of different pizzas

---
+++ components/PizzaList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard";
+
+const PizzaList = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        dolorem eligendi, alias doloribus numquam aperiam voluptatum voluptas.
+        Nihil, fugiat? Dolores laudantium sint incidunt voluptatibus sequi quam
+        aliquam, quod omnis id!
+      </p>
+      <div className={styles.wrapper}>
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+        <PizzaCard />
+      </div>
+    </div>
+  );
+};
+
+export default PizzaList;
